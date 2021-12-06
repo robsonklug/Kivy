58e6e450b483d358c91fd2f258f72329222c7719
Robson Klug <robsonklug@gmail.com>
2021-12-06 10:59:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ class MeuAplicativo(App):
         self.root.ids["moeda2"].text = f"Euro R$ {self.pegar_cotacao('EUR')}"
         self.root.ids["moeda3"].text = f"Bitcoin R$ {self.pegar_cotacao('BTC')}"
         self.root.ids["moeda4"].text = f"Ethereum R$ {self.pegar_cotacao('ETH')}"
+        
 
-    def pegar_cotacao(self, moeda):
-        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
-        requisicao = requests.get(link)
-        dic_requisicao = requisicao.json()
-        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
-        return cotacao
+    def pegar_cotacao(self, currency):
+        link           = f"https://economia.awesomeapi.com.br/last/{currency}-BRL"
+        x_request      = requests.get(link)
+        x_request_json = x_request.json()
+        currency = x_request_json[f"{currency}BRL"]["bid"]
+        return currency
 
 
 MeuAplicativo().run()
